5848fe3ed20c1a4f0974cc56100974641f962360
unknown <adi120.a@gmail.com>
2019-08-11 17:39:23 +0200

    New changes ...

---
+++ docs/precache-manifest.7849d8282d0e7ced824d94cf5ceefdaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053db099a747078418441e92128e550e",
+    "url": "/e-remind/index.html"
+  },
+  {
+    "revision": "813d336bbf3838fdcad1",
+    "url": "/e-remind/static/js/2.020737a4.chunk.js"
+  },
+  {
+    "revision": "2c4ad0b699c2f04d22de",
+    "url": "/e-remind/static/js/main.7d04e1ee.chunk.js"
+  },
+  {
+    "revision": "91a0d9543c59df6eccec",
+    "url": "/e-remind/static/js/runtime~main.c52397a7.js"
+  },
+  {
+    "revision": "77d8de19bf3b73a6c765010c07ace080",
+    "url": "/e-remind/static/media/background.77d8de19.svg"
+  }
+]);
